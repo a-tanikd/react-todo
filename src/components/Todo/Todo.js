15cc2fa81e0ce48206f6fdcec96fecacda9cfdf2
a-tanikd <8575113+a-tanikd@users.noreply.github.com>
2019-12-27 23:07:19 +0900

    Rename checked -> done

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -5,7 +5,7 @@ import React from 'react'
 function Todo (props) {
   return (
     <ListItem divider={props.divider}>
-      <Checkbox onClick={props.onCheckBoxToggle} checked={props.checked} disableRipple />
+      <Checkbox onClick={props.onCheckBoxToggle} checked={props.done} disableRipple />
       <ListItemText primary={props.text} />
       <ListItemSecondaryAction>
         <IconButton aria-label="Delete Todo" onClick={props.onButtonClick}>
